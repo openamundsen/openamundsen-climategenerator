Fix calculation of minimum distances for all-NaN slices

openamundsen_climategenerator/climategenerator.py
@@ -11,7 +11,13 @@ from .conf import parse_config
 
 def min_dist(target_temp, temps, target_precip, precips):
     dists = np.sqrt((temps - target_temp)**2 + (precips - target_precip)**2).values
-    year_idx, slice_idx = np.unravel_index(np.nanargmin(dists), dists.shape)
+
+    try:
+        argmin = np.nanargmin(dists)
+    except ValueError:  # (in case of all-NaN arrays)
+        argmin = 0
+
+    year_idx, slice_idx = np.unravel_index(argmin, dists.shape)
     selected_year = temps.year.values[year_idx]
     selected_slice = temps.slice.values[slice_idx]
     return (selected_year, selected_slice)